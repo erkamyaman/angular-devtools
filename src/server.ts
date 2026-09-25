9113590c8a4631acf293029ef526ce17951428ad
Santosh Yadav <santosh@coderabbit.ai>
2026-09-25 18:13:55 +0200

    fix format

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,12 @@ const app = express();
 const angularApp = new AngularNodeAppEngine();
 
 const auth = process.env['NG_DEVTOOLS_AUTH'] === 'true';
-const devtools = initDevframe(ngDevtools, { base: '/__ng-devtools/', ws: false, auth, allowedOrigins: false });
+const devtools = initDevframe(ngDevtools, {
+  base: '/__ng-devtools/',
+  ws: false,
+  auth,
+  allowedOrigins: false,
+});
 app.use(devtools.nodeMiddleware);
 
 /**
