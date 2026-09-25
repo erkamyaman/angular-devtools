14cfa0f6f0bc36e0ff0ea00d2167c569a0aea589
Santosh Yadav <santosh@coderabbit.ai>
2026-09-25 17:20:18 +0200

    use websocket transport for devframe

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const angularApp = new AngularNodeAppEngine();
 
 const auth = process.env['NG_DEVTOOLS_AUTH'] === 'true';
-const devtools = initDevframe(ngDevtools, { base: '/__ng-devtools/', ws: false, auth });
+const devtools = initDevframe(ngDevtools, { base: '/__ng-devtools/', auth });
 app.use(devtools.nodeMiddleware);
 
 /**
